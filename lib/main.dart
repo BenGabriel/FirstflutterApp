477ae177f505e67071d9efc22bea4fa131bf2031
unknown <simeongabriel175@gmail.com>
2022-02-28 07:28:55 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, avoid_print
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, avoid_print, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -6,7 +6,16 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool myNewButton = false;
+  String myText = 'Hello';
+  num check = 2;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,6 +23,32 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           centerTitle: true,
           title: Text('Flutter Coding Excersise'),
+        ),
+        body: Column(
+          crossAxisAlignment: myNewButton ? CrossAxisAlignment.start : CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  myNewButton = !myNewButton;
+                });
+              },
+              child: Text('My Button'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.black, onPrimary: Colors.blue),
+            ),
+            Container(
+              height: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(myNewButton ? "This is true" : myText),
+                ],
+              ),
+            )
+          ],
         ),
         bottomNavigationBar: BottomAppBar(
           color: Colors.blue,
