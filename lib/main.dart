cb4f70f881fe4292855a4e5d689875e3dd4d10d4
unknown <simeongabriel175@gmail.com>
2022-02-28 03:52:00 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, avoid_print
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,70 +7,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: FirstPage());
-  }
-}
-
-class FirstPage extends StatelessWidget {
-  FirstPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              // MaterialPageRoute(builder: (context) {
-              //   return SecondPage();
-              // }),
-              // CupertinoPageRoute(builder: (context) {
-              //   return SecondPage();
-              // }),
-              PageRouteBuilder(
-                pageBuilder: (c, a1, a2) => SecondPage(),
-                transitionsBuilder: (c, a1, a2, child) => FadeTransition(
-                  opacity: a1,
-                  child: child,
-                ),
-              ),
-            );
-          },
-          child: Text('First Page'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Flutter Coding Excersise'),
         ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  SecondPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) {
-                return FirstPage();
-              }),
-            );
-          },
-          child: Text('Second Page'),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
